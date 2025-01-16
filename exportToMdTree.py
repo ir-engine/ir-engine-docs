4beb0a2403be8a523c91280846c7df17a7ea1b65
Daniel Belmes <danielbelmes@ymail.com>
2025-01-16 11:49:00 -0800

    implement programatic tree

---
+++ exportToMdTree.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from directory_tree import DisplayTree
+
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep) -1
+        indent = ' ' * 2 * (level)
+        basename = os.path.basename(root)
+        relpath = os.path.relpath(root)
+        if "visualscript" in relpath: 
+                continue
+        if "creator" in relpath: 
+                continue
+        if "_partials" in relpath: 
+                continue
+        print('{}- [{}]({}/{}.md) '.format(indent, basename, relpath, basename))
+        subindent = ' ' * 2 * (level + 1)
+        for f in files:
+            if ".yml" in f: 
+                continue
+            print('{}- [{}]({}/{}) '.format(subindent, f, relpath, f))
+
+
+def main() -> int:
+    list_files('.')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
